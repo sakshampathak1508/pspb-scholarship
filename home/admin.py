00f97e2d8664c9d19d58de462ac32487c65d6aac
saksham15 <saksham.pathak@pocketfm.com>
2023-03-29 21:05:48 +0530

    admin filter

--- home/admin.py
+++ home/admin.py
@@ -1,19 +1,43 @@
 from django.contrib import admin
 from . models import Scholarship,SportCertificates,SportsAchievements
-# Register your models here.
-# class Scholarshipadmin(admin.ModelAdmin):
-#     list_display = ["related_ach","related_cert","name"] 
+from django.utils.html import format_html
 
-# admin.site.register(Scholarship, Scholarshipadmin)
-class SportsAchievementsInline(admin.StackedInline):
+class SportsAchievementsInline(admin.TabularInline):
     model = SportsAchievements
 
-class SportCertificatesInline(admin.StackedInline):
+class SportCertificatesInline(admin.TabularInline):
     model = SportCertificates
 
 class ScholarshipAdmin(admin.ModelAdmin):
     inlines = [SportsAchievementsInline,SportCertificatesInline]
+    list_display = ('name','sport' ,'category','gender','state','playing_position','view','download')
+    search_fields = ('name','sport' ,'category','gender','state','playing_position')
+    # list_editable = ('name','sport' ,'category','gender','state','playing_position')
+    ordering = ('-date',)
+    list_per_page = 25
+    list_filter = ('name','sport' ,'category','gender','state','playing_position')
+    def view(self,obj):
+        return format_html(f'<a href="#" class="default">View Data</a>')
+    def download(self,obj):
+        return format_html(f'<a href="#" class="default">Download PDF</a>')
 
-admin.site.register(SportCertificates)
-admin.site.register(SportsAchievements)
+
 admin.site.register(Scholarship,ScholarshipAdmin)
+
+@admin.register(SportsAchievements)
+class SaAdmin(admin.ModelAdmin):
+    list_display = ('scholar','tournament_name' ,'venue','date_from','date_to','achievement','event_type',)
+    search_fields = ('scholar','tournament_name' ,'venue','date_from','date_to','event_type')
+    # list_editable = ('name','sport' ,'category','gender','state','playing_position')
+    ordering = ('-scholar',)
+    list_per_page = 25
+    list_filter = ('scholar','tournament_name' ,'venue','date_from','date_to','event_type')
+
+@admin.register(SportCertificates)
+class ScAdmin(admin.ModelAdmin):
+    list_display = ('scholar','certificate')
+    search_fields = ('scholar',)
+    # list_editable = ('name','sport' ,'category','gender','state','playing_position')
+    ordering = ('-scholar',)
+    list_per_page = 25
+    list_filter = ('scholar',)
--- home/views.py
+++ home/views.py
@@ -15,6 +15,71 @@ from rest_framework import status
 from rest_framework import pagination,generics
 
 
+# test_data = {
+#     "main_data": {
+#         "name": "virat kohli",
+#         "sport": "cricket",
+#         "playing_position": "winner",
+#         "category": "senior",
+#         "level": "national",
+#         "is_ews": True,
+#         "gender": "male",
+#         "parent_name": "Mr Kohli",
+#         "mobile_number": 9818182074,
+#         "aadhar_number": 274714716693,
+#         "pan_number": "GMUPP4936T",
+#         "address": "221 B Baker Street",
+#         "district": "Palam Vihar",
+#         "state": "Haryana",
+#         "pin_code": 122017,
+#         "educational_details": "BA PASS",
+#         "other_achievements": "Winner of IPL",
+#         "place": "New Delhi",
+#         "date": "2023-03-28"
+#     },
+#     "achivements": [
+#         {
+#             "tournament_name": "World cup",
+#             "venue": "Austraila",
+#             "date_from": "2022-03-28",
+#             "date_to": "2022-04-28",
+#             "achievement": "Winner",
+#             "event_type": "international",
+#         },
+#         {
+#             "tournament_name": "IPL",
+#             "venue": "India",
+#             "date_from": "2023-03-28",
+#             "date_to": "2023-04-28",
+#             "achievement": "Winner",
+#             "event_type": "national",
+#         }
+#     ],
+#     "certificates" : []
+# }
+
+# "photo": "/media/documents/photo/licha.pdf",
+# "sign": "/media/documents/sign/licha.pdf",
+# "birth_certificate": "/media/documents/birth-cert/licha.pdf",
+# "aadhar": "/media/documents/aadhar/licha.pdf",
+# "pan": "/media/documents/pan/licha.pdf",
+# "passport": "/media/documents/passport/licha.pdf",
+
+# "certificates": [
+    #     {
+    #         "id": 1,
+    #         "certificate": "/media/certificates/licha.pdf",
+    #         "scholar": 1
+    #     },
+    #     {
+    #         "id": 2,
+    #         "certificate": "/media/certificates/licha_TxmBC7o.pdf",
+    #         "scholar": 1
+    #     }
+    # ]
+
+
+
 class AllScholarView(APIView):
     parser_classes = (MultiPartParser, FormParser, )
     def get(self, request,*args, **kwargs):
@@ -24,23 +89,25 @@ class AllScholarView(APIView):
     
     def post(self, request, format=None):
         data = request.data
-        main_data = data['main']
+        # data = test_data
+        main_data = data['main_data']
         serializer = ScholarSerializers(data=main_data)
         if serializer.is_valid():
             obj = serializer.save()
-            for i in data['sports_achievements']:
+            for i in data['achivements']:
+                i['scholar'] = obj.id
                 ach_obj = AchivementSerializers(data=i)
-                ach_obj.scholar = obj
-                ach_obj.save()
-            for i in data['sports_certificates']:
+                if ach_obj.is_valid():
+                    ach_obj.save()
+            for i in data['certificates']:
                 certs = dict((request.FILES).lists()).get('certificate', None)
                 for c in certs:
                     cert_data = {}
                     cert_data["certificate"] = c
+                    cert_data['scholar'] = obj.id
                     cert_obj = CertificateSerializers(data=cert_data)
                     if cert_obj.is_valid():
-                        cert_obj.scholar = obj
-                        serializer.save()
+                        cert_obj.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
@@ -57,7 +124,7 @@ class AllDataTabularFormView(APIView):
             data['main_data'] = obj_ser.data
 
             ach_ser = AchivementSerializers(ach_obj,many=True)
-            data['achivement'] = ach_ser.data
+            data['achivements'] = ach_ser.data
 
             cert_ser = CertificateSerializers(cert_obj,many=True)
             data['certificates'] = cert_ser.data
