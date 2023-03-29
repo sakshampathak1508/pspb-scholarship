971c60ab5fb5449d9b3f991e616f47fb0db475a0
saksham15 <saksham.pathak@pocketfm.com>
2023-03-29 12:38:32 +0530

    init

--- pspb/settings.py
+++ pspb/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_25teotfu!=0rfra(y*i%=_9h+n%t#5kug0ibgkw=xis+)^*ex'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
